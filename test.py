cbd324cb6dbc0ad880c3d0e0cd52617dcb2fca85
Chanwoo Lee <lonwolf@dreamwiz.com>
2016-01-05 15:05:19 -0500

    third copy

--- test.py
+++ test.py
@@ -2,3 +2,10 @@ from scrape import *
 s.go('http://finfra.com/w')
 print s.headers
 print s.content[:100]
+
+from scrape import *
+s.go('http://finfra.com/w')
+print s.headers
+print s.content[:100]
+
+tested 2016.01.05
